cb9b60c5c923594cefdb465c0fac3c6ec1ab1a8f
gauravanand-sudo <ga.oct.2024@gmail.com>
2026-01-02 20:27:40 +0530

    4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -52,8 +51,6 @@ export default function RootLayout({
           </div>
         </footer>
         
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
